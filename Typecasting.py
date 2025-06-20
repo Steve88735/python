3bebca9c004c7c486856a4c49f0132640ac2e7a1
Steve88735 <ppoppa0817@gmail.com>
2025-06-20 22:48:45 +0900

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,15 @@
+name="penguin" 
+age= 15
+is_student = True 
+weight = 45.5 
+print("Name:",name, "Type:", type(name))
+print("Age:",age, "Type:",type(age))
+print("Is Student:",is_student, "Type:",type(is_student))
+print ("Weight:",weight,"Type:",type(weight))
+print("\nTypecasting Examples")
+#converting string to integer
+age=str(age)
+print("Age after typecasting to string:",age,"Type:",type(age))  
+#converting float to integer 
+weight= int (weight)
+print("Weight after typecasting to integer:",weight,"Type:",type(weight))
