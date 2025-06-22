0bfd2ed40f07e71b9561fc19fe194fdeaa3947b3
Steve88735 <ppoppa0817@gmail.com>
2025-06-22 22:30:18 +0900

    Add files via upload

---
+++ variablehw.py
@@ -0,0 +1,8 @@
+for i in range(5):
+    print
+    name = input("Enter your name: ")
+    birthday= input("Enter your birthday:")
+    print("\nHello",name,"your birthday is",birthday)
+    
+
+    
